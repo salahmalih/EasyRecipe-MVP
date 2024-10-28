5860d078bfa02666fe0fac6d0ade89f022bca178
salahmalih <c130084552@taalim.ma>
2024-10-28 20:56:57 +0100

    add all missing files

---
+++ src/page/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home';
+export { default as Recipes } from './Recipes/Recipes';
+export { default as Favourite } from './Favourite/Favourite';
+export { default as NoPage } from './NoPage/NoPage';
