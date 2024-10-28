4437302db84481f591bef66d396a87d7ec29d249
salahmalih <c130084552@taalim.ma>
2024-10-28 20:47:51 +0100

    add files affiche all recipe

---
+++ src/components/allrecipe/AllRecipe.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Article } from '../';
+import './allrecipe.css';
+
+const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+const truncateDescription = (description, maxLength) => {
+  if (description.length <= maxLength) {
+    return description;
+  }
+  return `${description.substring(0, maxLength)}...`;
+};
+
+const AllRecipe = ({ recipes }) => {
+  const firstRecipe = recipes[0];
+  const secondRecipe = recipes[1];
+  const remainingRecipes = recipes.slice(2);
+
+  return (
+    <div className="recipe__recipe section__padding" id="recipe">
+      <div className="recipe__recipe-heading">
+        <h1 className="gradient__text">ALL Recipes</h1>
+      </div>
+      <div className="recipe__recipe-container">
+        <div className="recipe__recipe-container_groupA">
+          {firstRecipe && (
+            <div className="recipe__recipe-container_groupA_1">
+              <Article
+                imgUrl={`data:image/png;base64,${firstRecipe.image}`}
+                date={formatDate(firstRecipe.date)}
+                title={firstRecipe.title}
+                description={truncateDescription(firstRecipe.description, 100)}
+              />
+            </div>
+          )}
+          {secondRecipe && (
+            <div className="recipe__recipe-container_groupA_2">
+              <Article
+                imgUrl={`data:image/png;base64,${secondRecipe.image}`}
+                date={formatDate(secondRecipe.date)}
+                title={secondRecipe.title}
+                description={truncateDescription(secondRecipe.description, 100)}
+              />
+            </div>
+          )}
+        </div>
+        <div className="recipe__recipe-container_groupB">
+          {remainingRecipes.map(recipe => (
+            <div key={recipe.recipe_id} className="recipe__recipe-item">
+              <Article
+                imgUrl={`data:image/png;base64,${recipe.image}`}
+                date={formatDate(recipe.date)}
+                title={recipe.title}
+                description={truncateDescription(recipe.description, 100)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllRecipe;
