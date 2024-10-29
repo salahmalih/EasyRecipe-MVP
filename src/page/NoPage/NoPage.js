5805be9872e9fa9f8208c8d4a43bc5073f4a58c6
salahmalih <c130084552@taalim.ma>
2024-10-29 21:28:11 +0100

    Update Not found

--- src/page/NoPage/NoPage.js
+++ src/page/NoPage/NoPage.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nopage.css'; // Import your CSS file
 
-function NoPage  ()  {
+const NoPage = () => {
   return (
-    <div>
-      <h1 >NoPage</h1>
+    <div className="nopage-container">
+      <h1 className="nopage-title">404 - Page Not Found</h1>
+      <p className="nopage-message">Sorry, the page you are looking for does not exist.</p>
+      <Link to="/" className="nopage-home-button">Go to Home</Link>
     </div>
-  )
-}
+  );
+};
 
-export default NoPage
+export default NoPage;
